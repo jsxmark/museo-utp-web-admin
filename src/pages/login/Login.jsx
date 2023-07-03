f00842fc399576ef97ca6483da986ada9965963c
jsxmark <jsxmark@gmail.com>
2023-07-03 16:13:09 -0500

    umrisi
    primeros estilos

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,12 @@
 import '../../styles/login.css';
-import Menu
+import '../../styles/normalize.css';
 
 function Login() {
     return (
-    <div className="App">
-        <h1>Login works</h1>
-    </div>
+        <div className="main-container">
+            <div className="left">Contenido a la izquierda</div>
+            <div className="right">Contenido a la derecha</div>
+        </div>
     );
 }
 
