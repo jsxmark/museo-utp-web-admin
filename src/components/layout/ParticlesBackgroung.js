4bed2e8e8e73021ee65b8bea52c6f8cd9aed904d
jsxmark <jsxmark@gmail.com>
2023-07-04 11:19:25 -0500

    img back right
    .

---
+++ src/components/layout/ParticlesBackgroung.js
@@ -0,0 +1,9 @@
+import Particles from "react-tsparticles";
+import ParticlesConfig from "./particles-config";
+const ParticlesBackgroung = () =>{
+    return(
+        <Particles params={ParticlesConfig}></Particles>
+    );
+}
+
+export default ParticlesBackgroung
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,13 @@
 import '../../styles/login.css';
 import '../../styles/normalize.css';
+import ParticlesBackgroung from "../../components/layout/ParticlesBackgroung";
 
 function Login() {
     return (
         <div className="main-container">
             <div className="left"></div>
             <div className="right">
+                <ParticlesBackgroung/>
                 <div className="right-container-form">
                     <h2 className='login-text degradado-verde'>Inicio de Sesión</h2>
                     <p className='welcome-text'>¡Bienvenido de nuevo! Inicia sesión para acceder a tu cuenta</p>
