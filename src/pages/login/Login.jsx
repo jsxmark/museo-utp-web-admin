13a34411031c4c291547aa9d4846b44a52cebf32
irving_javier15 <bbjavier09@hotmail.com>
2023-07-07 23:42:50 -0500

    .

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import '../../styles/login.css';
 import '../../styles/normalize.css';
 import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom"
-import axios from 'axios';
+import { AuthenticateService } from "../../services/authenticate.service";
 
 function Login() {
 
@@ -12,34 +12,48 @@ function Login() {
     const navigate = useNavigate();
 
     const handleSubmit = (event) => {
-        event.preventDefault();
+        event.preventDefault()
         if (email && password) {
-            axios.post('http://localhost:3000/user', { email, password })
-          .then((response) => {
-              console.log(response.data)
-              navigate("/dashboard");
-              window.location.reload();
-          })
-          .catch((error) => {
-              console.error(error);
-              setError('Error al iniciar sesión. Por favor, verifica tus credenciales.');
-          });
+            AuthenticateService.login(email, password)
+            .then(() => {
+                navigate("/dashboard")
+                window.location.reload()
+            })
+            .catch(() => {
+                setError('Error en la solicitud de inicio de sesión. Por favor, inténtalo nuevamente.')
+            }).finally(() => {
+                setTimeout(() => {
+                    setError('')
+                }, 3000);
+            });
         } else {
-            setError('Por favor, ingresa tu usuario y contraseña.');
+            setError('Por favor, ingresa tu usuario y contraseña.')
         }
-    };
+    }
 
     return (
         <div className="main-container">
-            <div className="left"></div>
+            <div className="left">
+            <div className="right-container-form-responsive flex-form">
+                    {error && <div className= 'error-title'>{error}</div>}
+                    <form className='login-form'onSubmit={handleSubmit}>
+                        <h2 className='login-text degradado-verde'>Inicio de Sesión</h2>
+                        <p className='welcome-text'>¡Bienvenido de nuevo! Inicia sesión para acceder a tu cuenta</p>
+                        <input className='log-input' type="email" placeholder='Correo electrónico' value={email} onChange={(event) => setEmail(event.target.value)}/>
+                        <input className='log-input' type="password" placeholder='Contraseña' value={password} onChange={(event) => setPassword(event.target.value)}/>
+                        <p className='signup-prompt'><Link to={"/register"} >¿Aún no tienes una cuenta?</Link></p>
+                        <button className='btn-login' type='submit'>Iniciar Sesión</button>
+                    </form>
+                </div>
+            </div>
             <div className="right">
                 <div className="right-container-form">
                     {error && <div className= 'error-title'>{error}</div>}
-                    <form onSubmit={handleSubmit}>
+                    <form className='login-form'onSubmit={handleSubmit}>
                         <h2 className='login-text degradado-verde'>Inicio de Sesión</h2>
                         <p className='welcome-text'>¡Bienvenido de nuevo! Inicia sesión para acceder a tu cuenta</p>
-                        <input type="email" placeholder='Correo electrónico' value={email} onChange={(event) => setEmail(event.target.value)}/>
-                        <input type="password" placeholder='Contraseña' value={password} onChange={(event) => setPassword(event.target.value)}/>
+                        <input className='log-input' type="email" placeholder='Correo electrónico' value={email} onChange={(event) => setEmail(event.target.value)}/>
+                        <input className='log-input' type="password" placeholder='Contraseña' value={password} onChange={(event) => setPassword(event.target.value)}/>
                         <p className='signup-prompt'><Link to={"/register"} >¿Aún no tienes una cuenta?</Link></p>
                         <button className='btn-login' type='submit'>Iniciar Sesión</button>
                     </form>
