2d989038540c4c8803f15f6e835ac460d9380c84
irving_javier15 <bbjavier09@hotmail.com>
2023-07-22 01:21:45 -0500

    .

--- src/pages/articles/Articles.jsx
+++ src/pages/articles/Articles.jsx
@@ -1,19 +1,30 @@
-import '../../styles/articles.css';
-import '../../styles/dashboard.css'
-import data from "../../utils/data.json"
-import '../../styles/normalize.css';
-import {nanoid} from "nanoid"
-// import { useState} from "react"
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
 import LeerLinea from '../../components/common/LeerLinea';
 import EditarLinea from '../../components/common/EditarLinea';
 import SideBar from '../../components/common/SideBar';
 import SideBarResponsive from '../../components/common/SideBarResponsive';
-import React, { useState, useEffect } from 'react';
 import { ArticlesService } from '../../services/articles.service';
 
 function Articles() {
+  const [articles, setArticles] = useState([]);
+  const [addFormData, setAddFormData] = useState({
+    nombre: '',
+    ubicacion: '',
+    categoria: '',
+    descripcion: '',
+    dueno: '',
+  });
 
-    const [articles, setArticles] = useState([]);
+  const [editarData, setEditarData] = useState({
+    nombre: '',
+    ubicacion: '',
+    categoria: '',
+    descripcion: '',
+    dueno: '',
+  });
+
+  const [editarArticuloId, setEditarArticuloId] = useState(null);
 
   useEffect(() => {
     ArticlesService.getArticles()
@@ -21,212 +32,198 @@ function Articles() {
       .catch((error) => console.error(error));
   }, []);
 
-    const [articulos, setArticulos] = useState(data);
-    const [addFormData, setAddFormData] = useState({
-        nombre: "",
-        ubicacion: "",
-        categoria: "",
-        descripcion: "",
-        dueno: "",
+  const handleEditarFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute('name');
+    const fieldValue = event.target.value;
+
+    const newData = { ...editarData };
+    newData[fieldName] = fieldValue;
+
+    setEditarData(newData);
+  };
+
+  const handleClicEditar = (event, articulo) => {
+    event.preventDefault();
+    setEditarArticuloId(articulo.idTemporal);
+
+    const formValues = {
+      nombre: articulo.nombre,
+      ubicacion: articulo.ubicacion,
+      categoria: articulo.categoria,
+      dueno: articulo.dueno,
+      descripcion: articulo.descripcion,
+    };
+
+    setEditarData(formValues);
+  };
+
+  const handleEditarSubmit = (event) => {
+    event.preventDefault();
+
+    const articuloEditado = {
+      idTemporal: editarArticuloId,
+      nombre: editarData.nombre,
+      ubicacion: editarData.ubicacion,
+      categoria: editarData.categoria,
+      dueno: editarData.dueno,
+      descripcion: editarData.descripcion,
+    };
+
+    const newArticles = articles.map((articulo) =>
+      articulo.idTemporal === editarArticuloId ? articuloEditado : articulo
+    );
+
+    setArticles(newArticles);
+    setEditarArticuloId(null);
+  };
+
+  const handleCancelar = () => {
+    setEditarArticuloId(null);
+  };
+
+  const handleEliminar = () => {
+    const newArticles = articles.filter(
+      (articulo) => articulo.idTemporal !== editarArticuloId
+    );
+
+    setArticles(newArticles);
+    setEditarArticuloId(null);
+  };
+
+  const handleFormChange = (event) => {
+    event.preventDefault();
+
+    const nombre = event.target.getAttribute('name');
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addFormData };
+    newFormData[nombre] = fieldValue;
+
+    setAddFormData(newFormData);
+  };
+
+  const handleFromSubmit = (event) => {
+    event.preventDefault();
+
+    const newArticulo = {
+      idTemporal: nanoid(),
+      nombre: addFormData.nombre,
+      ubicacion: addFormData.ubicacion,
+      categoria: addFormData.categoria,
+      descripcion: addFormData.descripcion,
+      dueno: addFormData.dueno,
+    };
+
+    const newArticles = [...articles, newArticulo];
+    setArticles(newArticles);
+
+    // Limpiar el formulario después de agregar un artículo
+    setAddFormData({
+      nombre: '',
+      ubicacion: '',
+      categoria: '',
+      descripcion: '',
+      dueno: '',
     });
+  };
 
-    const [editarData, setEditarData] = useState({
-        nombre: "",
-        ubicacion: "",
-        categoria: "",
-        descripcion: "",
-        dueno: "",
-    })
-    const [editarArticuloId, setEditarArticuloId] = useState(null);
-
-    //Función para manejar los cambios en el formulario al editar un articulo
-    const handleEditarFormChange = (event) => {
-        event.preventDefault();
-
-        const fieldName = event.target.getAttribute("name");
-        const fieldValue = event.target.value;
-
-        const newData = {...editarData}
-        newData[fieldName] = fieldValue;
-
-        setEditarData(newData);
-    }
-
-    
-    const handleClicEditar = (event, articulo) => {
-        event.preventDefault();
-        setEditarArticuloId(articulo.idTemporal);
-
-        const formValues = {
-            nombre: articulo.nombre,
-            ubicacion: articulo.ubicacion,
-            categoria: articulo.categoria,
-            dueno: articulo.dueno,
-            descripcion: articulo.descripcion
-        }
-
-        setEditarData(formValues);
-    }
-
-    const handleEditarSubmit = (event) => {
-        event.preventDefault();
-
-        const articuloEditado = {
-            id: editarArticuloId,
-            nombre: editarData.nombre,
-            ubicacion: editarData.ubicacion,
-            categoria: editarData.categoria,
-            dueno: editarData.dueno,
-            descripcion: editarData.descripcion
-        }
-
-        const newArticulos = [...articulos];
-
-        const index = articulos.findIndex((articulo) => articulo.idTemporal === editarArticuloId);
-
-        newArticulos[index] = articuloEditado;
-
-        setArticulos(newArticulos);
-        setEditarArticuloId(null)
-    };
-
-
-    
-    const handleCancelar = () => {
-        setEditarArticuloId(null);
-    }
-
-    const handleEliminar = () => {
-        const newArticulo = [...articulos];
-
-        const index = articulos.findIndex((articulo) => articulo.idTemporal === editarArticuloId);
-
-        newArticulo.splice(index, 1);
-
-        setArticulos(newArticulo);
-    }
-
-    const handleFormChange = (event) => {
-        event.preventDefault();
-
-        const nombre = event.target.getAttribute("name");
-        const fieldValue = event.target.value;
-
-        const newFormData = {...addFormData};
-        newFormData[nombre] = fieldValue;
-
-        setAddFormData(newFormData);
-    };
-
-    const handleFromSubmit = (event) => {
-        event.preventDefault();
-
-        const newArticulo = {
-            id: nanoid(),
-            nombre: addFormData.nombre,
-            ubicacion: addFormData.ubicacion,
-            categoria: addFormData.categoria,
-            descripcion: addFormData.descripcion,
-            dueno: addFormData.dueno,
-        };
-
-        const newArticulos = [...articulos, newArticulo];
-        setArticulos(newArticulos);
-    };
-
-    return (
-        <div className='container'>
-            <SideBar/>
-    
-            <section className="main-articles">
-                
-                <SideBarResponsive/>
-                
-                <h1>Lista de Articulos</h1>
-                <form onSubmit={handleEditarSubmit}>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Nombre</th>
-                                <th>Ubicacion</th>
-                                <th>Categoria</th>
-                                <th>Dueño</th>
-                                <th>Descripcion</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-  {articles.map((articulo) => (
-    <>
-      {editarArticuloId === articulo.idTemporal ? (
-        <EditarLinea
-          editarData={editarData}
-          handleEditarFormChange={handleEditarFormChange}
-          handleCancelar={handleCancelar}
-        />
-      ) : (
-        <LeerLinea
-          articulo={articulo}
-          handleClicEditar={handleClicEditar}
-          handleEliminar={handleEliminar}
-        />
-      )}
-    </>
-  ))}
-</tbody>
-                    </table>
-                </form>
-
-                <h2>Agregar Objeto</h2>
-
-                <form onSubmit={handleFromSubmit}>
-                    <input
-                        className="article-input"
-                        type="text" 
-                        name="nombre"
-                        required
-                        placeholder="Ingrese Nombre..."
-                        onChange={handleFormChange}
+  return (
+    <div className='container'>
+      <SideBar />
+      <section className='main-articles'>
+        <SideBarResponsive />
+        <h1>Lista de Articulos</h1>
+        <form onSubmit={handleEditarSubmit}>
+          <table>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Ubicacion</th>
+                <th>Categoria</th>
+                <th>Dueño</th>
+                <th>Descripcion</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {articles.map((articulo) => (
+                <React.Fragment key={articulo.idTemporal}>
+                  {editarArticuloId === articulo.idTemporal ? (
+                    <EditarLinea
+                      editarData={editarData}
+                      handleEditarFormChange={handleEditarFormChange}
+                      handleCancelar={handleCancelar}
                     />
-                    <input
-                        className="article-input"
-                        type="text" 
-                        name="ubicacion"
-                        required
-                        placeholder="Se encuentra en..."
-                        onChange={handleFormChange}
+                  ) : (
+                    <LeerLinea
+                      articulo={articulo}
+                      handleClicEditar={handleClicEditar}
+                      handleEliminar={handleEliminar}
                     />
-                    <input
-                        className="article-input"
-                        type="text" 
-                        name="categoria"
-                        required
-                        placeholder="Ingrese Categoria..."
-                        onChange={handleFormChange}
-                    />
-                    <input
-                        className="article-input"
-                        type="text" 
-                        name="dueno"
-                        required
-                        placeholder="Pertenece a...?"
-                        onChange={handleFormChange}
-                    />
-                    <input
-                        className="article-input"
-                        type="text" 
-                        name="descripcion"
-                        required
-                        placeholder="Descripcion..."
-                        onChange={handleFormChange}
-                    />
-                    <button className="article-button" type="submit">Agregar</button>
-                </form>
-            </section>
+                  )}
+                </React.Fragment>
+              ))}
+            </tbody>
+          </table>
+        </form>
 
-        </div>
+        <h2>Agregar Objeto</h2>
 
-        );
+        <form onSubmit={handleFromSubmit}>
+          <input
+            className='article-input'
+            type='text'
+            name='nombre'
+            required
+            placeholder='Ingrese Nombre...'
+            value={addFormData.nombre}
+            onChange={handleFormChange}
+          />
+          <input
+            className='article-input'
+            type='text'
+            name='ubicacion'
+            required
+            placeholder='Se encuentra en...'
+            value={addFormData.ubicacion}
+            onChange={handleFormChange}
+          />
+          <input
+            className='article-input'
+            type='text'
+            name='categoria'
+            required
+            placeholder='Ingrese Categoria...'
+            value={addFormData.categoria}
+            onChange={handleFormChange}
+          />
+          <input
+            className='article-input'
+            type='text'
+            name='dueno'
+            required
+            placeholder='Pertenece a...?'
+            value={addFormData.dueno}
+            onChange={handleFormChange}
+          />
+          <input
+            className='article-input'
+            type='text'
+            name='descripcion'
+            required
+            placeholder='Descripcion...'
+            value={addFormData.descripcion}
+            onChange={handleFormChange}
+          />
+          <button className='article-button' type='submit'>
+            Agregar
+          </button>
+        </form>
+      </section>
+    </div>
+  );
 }
 
-export default Articles
+export default Articles;
--- src/services/articles.service.js
+++ src/services/articles.service.js
@@ -13,6 +13,36 @@ const getArticles = async () => {
   }
 };
 
+const addArticle = async (article) => {
+    try {
+      const response = await axios.post(BASE_URL + "/articulos", article);
+      return response.data;
+    } catch (error) {
+      throw new Error("Error al agregar el artículo: " + error);
+    }
+  };
+  
+  const updateArticle = async (id, article) => {
+    try {
+      const response = await axios.put(BASE_URL + `/articulos/${id}`, article);
+      return response.data;
+    } catch (error) {
+      throw new Error("Error al actualizar el artículo: " + error);
+    }
+  };
+  
+  const deleteArticle = async (id) => {
+    try {
+      const response = await axios.delete(BASE_URL + `/articulos/${id}`);
+      return response.data;
+    } catch (error) {
+      throw new Error("Error al eliminar el artículo: " + error);
+    }
+  };
+
 export const ArticlesService = {
   getArticles,
+  addArticle,
+  updateArticle,
+  deleteArticle,
 };
